eaf12a25defa7e48491b367cfa0ac68c0fd174cf
Daniel Udsen <dudsen@gmail.com>
2016-05-18 20:13:18 +0100

    lets pull this under git just for the sake of storing it somewhere

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Just a toy project more for the sake of learning javascript then anything else. 
+
+Because you have to slap a licence on anything this is under: Apache License http://www.apache.org/licenses/LICENSE-2.0 
---
+++ ejs/time.js
@@ -0,0 +1,39 @@
+var url = location.protocol + '//' + location.hostname + '/ejs/time.php';
+function conv(timestamp){
+    return new Date(timestamp).toTimeString()}
+//setting local time
+function loc(){
+    var locdate = new Date().getTime();
+    return [locdate/1000, conv(locdate)]}
+//setting remote time
+
+function xhr(uri, callback) {
+    var start = new Date().getTime()/1000;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState == 4 && xhttp.status == 200 ) {
+            callback(xhttp.responseText, start);
+        }
+    };
+    xhttp.open("GET", uri, true);
+    xhttp.send();
+}
+
+function trigger (remtime, start) {
+    var outstring = conv(remtime*1000);
+    print(loc(),[remtime, outstring], start);
+}
+//printing to the dom based on the output from loc and rem
+function print(loct, remt, start){
+    document.getElementById("lt").innerHTML = loct[1];
+    if (remt[0] >0 )
+        { document.getElementById("rt").innerHTML = remt[1]; }
+    else
+        { document.getElementById("rt").innerHTML = 'remote connection failed' }
+    document.getElementById("diff").innerHTML = Math.abs((remt[0] - loct[0])).toPrecision(2);
+    document.getElementById("delay").innerHTML = Math.abs((loct[0] - start)).toPrecision(2);
+}
+
+function update() {
+    xhr(url, trigger);
+}
